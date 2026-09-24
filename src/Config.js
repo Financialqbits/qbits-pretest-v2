da5b684da4ff0e56e59a3e9b53e90fc5429f01af
Financialqbits <164356226+Financialqbits@users.noreply.github.com>
2026-09-24 11:09:54 -0600

    Fix EmailJS Public Key

--- src/Config.js
+++ src/Config.js
@@ -1,7 +1,7 @@
 export const APP_CONFIG = {
   EMAIL_SERVICE_ID: "service_c449oyl",
   EMAIL_TEMPLATE_ID: "template_rwe5fw4",
-  EMAIL_PUBLIC_KEY: "2L4l6tlIkzshKm8ul",
+  EMAIL_PUBLIC_KEY: "2L416tlIkzshKm8ul",
   PASSING_SCORE: 80,
   TIMER_MINUTES: 12
 };
